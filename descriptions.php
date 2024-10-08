f7b075d1c4c93d5983e2e4ab67932e7f2b39984c
Florian <105889597+florian-chicot@users.noreply.github.com>
2024-10-08 15:58:51 +0200

    ADD .gitignore, descriptions.php with static data

---
+++ descriptions.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Liste des Matchs</title>
+  <link rel="stylesheet" href="assets/styles/style.css">
+</head>
+<body>
+  <?php require_once 'header.php'; ?>
+  <main>
+    <section class="match-list">
+      <div class="card-container">
+        <div class="card">
+          <p>Basketball</p>
+          <p></p>
+          <p>Coupe de France</p>
+          <p>Toulouse vs Nantes</p>
+          <p><img src="assets\images\logo_equipe\BasketClubToulouse_2020-2023.svg" width="50">Toulouse vs Nantes<img src="assets\images\logo_equipe\BasketClubNantes_2017--.svg" width="50"></p>
+          <p>59 - 73</p>
+          <p>Petit Palais des Sports</p>
+        </div>
+        <div class="card">
+          <p>Rugby</p>
+          <p></p>
+          <p>Champions Cup</p>
+          <p><img src="assets\images\logo_equipe\RugbyClubToulouse_2016--.svg" width="50">Toulouse vs Ulster<img src="assets\images\logo_equipe\RugbyClubUlster_1999--.svg" width="50"></p>
+          <p>20 - 26</p>
+          <p>Stadium de Toulouse</p>
+        </div>
+        <div class="card">
+          <p>Basketball</p>
+          <p>Playoffs</p>
+          <p>Nationale 1</p>
+          <p><img src="assets\images\logo_equipe\BasketClubToulouse_2020-2023.svg" width="50">Toulouse vs Chartres<img src="assets\images\logo_equipe\BasketClubChartres_2018-2023.svg" width="50"></p>
+          <p>93 - 90</p>
+          <p>Petit Palais des Sports</p>
+        </div>
+      </div>
+    </section>
+  </main>
+  <?php require_once 'footer.php'; ?>
+</body>
+</html>
