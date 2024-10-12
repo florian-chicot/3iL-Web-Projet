32f3ebc095ce9f3a108e389dac5c421d63640792
Florian <105889597+florian-chicot@users.noreply.github.com>
2024-10-12 13:15:54 +0200

    UPDATE descriptions.php : match with updated database

--- descriptions.php
+++ descriptions.php
@@ -9,41 +9,41 @@
 <body>
   <?php require_once 'header.php'; ?>
   <main>
+    <?php
+    // Connexion à la base de données
+    $dsn = 'mysql:host=localhost;dbname=mes_matchs;charset=utf8';
+    $user = 'root';
+    $password = '';
 
-<?php
-// Connexion à la base de données
-$dsn = 'mysql:host=localhost;dbname=mes_matchs;charset=utf8';
-$user = 'root';
-$password = '';
+    try {
+        $pdo = new PDO($dsn, $user, $password);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch (PDOException $e) {
+        die('Erreur de connexion à la base de données : ' . $e->getMessage());
+    }
 
-try {
-    $pdo = new PDO($dsn, $user, $password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    die('Erreur de connexion à la base de données : ' . $e->getMessage());
-}
-
-// Requête pour récupérer les matchs et les informations associées
-$sql = "
-    SELECT 
-      matchs.date_match,
-      matchs.sport,
-      matchs.competition,
-      matchs.domicile_score,
-      matchs.visiteur_score,
-      matchs.affluence,
-      e1.nom AS equipe_domicile,
-      e1.ville AS logo_domicile,
-      e2.nom AS equipe_visiteur,
-      e2.ville AS logo_visiteur
-    FROM matchs
-    JOIN equipes e1 ON matchs.domicile_equipe = e1.id
-    JOIN equipes e2 ON matchs.visiteur_equipe = e2.id
-    ORDER BY matchs.date_match DESC
-";
-$stmt = $pdo->query($sql);
-$matchs = $stmt->fetchAll(PDO::FETCH_ASSOC);
-?>
+    // Requête pour récupérer les matchs et les informations associées
+    $sql = "
+        SELECT 
+          matchs.date_match,
+          matchs.sport,
+          matchs.domicile_score,
+          matchs.visiteur_score,
+          matchs.affluence,
+          e1.nom_court AS equipe_domicile,
+          e1.ville AS logo_domicile,
+          e2.nom_court AS equipe_visiteur,
+          e2.ville AS logo_visiteur,
+          competitions.nom AS competition
+        FROM matchs
+        JOIN equipes e1 ON matchs.domicile_equipe = e1.id
+        JOIN equipes e2 ON matchs.visiteur_equipe = e2.id
+        JOIN competitions ON matchs.competition = competitions.id
+        ORDER BY matchs.date_match DESC
+    ";
+    $stmt = $pdo->query($sql);
+    $matchs = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    ?>
     <section class="match-list">
       <div class="card-container">
         <?php foreach($matchs as $match): ?>
@@ -66,7 +66,6 @@ $matchs = $stmt->fetchAll(PDO::FETCH_ASSOC);
         <?php endforeach; ?>
     </div>
   </section>
-
   </main>
   <?php require_once 'footer.php'; ?>
 </body>
