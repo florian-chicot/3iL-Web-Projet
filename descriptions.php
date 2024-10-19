8f6bf09911de6cb5f7cb7f9e13a46857447fe227
Florian <105889597+florian-chicot@users.noreply.github.com>
2024-10-19 14:39:44 +0200

    UPDATE descriptions.php : Update the matches list

--- descriptions.php
+++ descriptions.php
@@ -25,15 +25,16 @@
     // Requête pour récupérer les matchs et les informations associées
     $sql = "
         SELECT 
+          matchs.id,
           matchs.date_match,
           matchs.sport,
           matchs.domicile_score,
           matchs.visiteur_score,
           matchs.affluence,
           e1.nom_court AS equipe_domicile,
-          e1.ville AS logo_domicile,
+          e1.logo AS logo_domicile,
           e2.nom_court AS equipe_visiteur,
-          e2.ville AS logo_visiteur,
+          e2.logo AS logo_visiteur,
           competitions.nom AS competition
         FROM matchs
         JOIN equipes e1 ON matchs.domicile_equipe = e1.id
@@ -45,27 +46,33 @@
     $matchs = $stmt->fetchAll(PDO::FETCH_ASSOC);
     ?>
     <section class="match-list">
-      <div class="card-container">
+      <ul class="card-container">  
+      <!-- <ul> -->
         <?php foreach($matchs as $match): ?>
-        <div class="card">
-          <div id="score-info" class="match-info">
-            <p id="equipe-domicile"><?= htmlspecialchars($match['equipe_domicile']) ?></p>
-            <img id="logo-equipe-domicile" src="assets/images/logo_equipe/<?= htmlspecialchars($match['sport']).'_'.htmlspecialchars($match['logo_domicile']) ?>.svg" class="team-logo" width="50">
-            <p id="score-domicile"><?= htmlspecialchars($match['domicile_score']) ?></p>
-            <p id="score-separateur">&nbsp;-&nbsp;</p>
-            <p id="score-visiteur"><?= htmlspecialchars($match['visiteur_score']) ?></p>
-            <img id="logo-equipe-visiteur" src="assets/images/logo_equipe/<?= htmlspecialchars($match['sport']).'_'.htmlspecialchars($match['logo_visiteur']) ?>.svg" class="team-logo" width="50">
-            <p id="equipe-visiteur"><?= htmlspecialchars($match['equipe_visiteur']) ?></p>
-          </div>
-          <div id="date-competition-info" class="match-info">
-            <p id="date" class="date"><?= date("d M Y", strtotime($match['date_match'])) ?></p>
-            <p id="equipe-separateur">&nbsp;|&nbsp;</p>
-            <p id="competition"><?= htmlspecialchars($match['competition']) ?></p>
-          </div>
-        </div>
+        <!-- <li> -->
+        <li class="card">
+          <a href="description.php?id=<?= $match['id'] ?>" class="card-link">
+            <div>
+              <div id="score-info" class="match-info">
+                <p id="equipe-domicile"><?= htmlspecialchars($match['equipe_domicile']) ?></p>
+                <img id="logo-equipe-domicile" src="assets/images/logo_equipe/<?= htmlspecialchars($match['logo_domicile']) ?>.svg" class="team-logo" width="50">
+                <p id="score-domicile"><?= htmlspecialchars($match['domicile_score']) ?></p>
+                <p id="score-separateur">&nbsp;-&nbsp;</p>
+                <p id="score-visiteur"><?= htmlspecialchars($match['visiteur_score']) ?></p>
+                <img id="logo-equipe-visiteur" src="assets/images/logo_equipe/<?= htmlspecialchars($match['logo_visiteur']) ?>.svg" class="team-logo" width="50">
+                <p id="equipe-visiteur"><?= htmlspecialchars($match['equipe_visiteur']) ?></p>
+              </div>
+              <div id="date-competition-info" class="match-info">
+                <p id="date" class="date"><?= date("d M Y", strtotime($match['date_match'])) ?></p>
+                <p id="equipe-separateur">&nbsp;|&nbsp;</p>
+                <p id="competition"><?= htmlspecialchars($match['competition']) ?></p>
+              </div>
+            </div>
+          </a>
+        </li>
         <?php endforeach; ?>
-    </div>
-  </section>
+      </ul>
+    </section>
   </main>
   <?php require_once 'footer.php'; ?>
 </body>
