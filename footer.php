95fc821ea23b7368dc4ebf15484dfd71cc013cc2
Florian <105889597+florian-chicot@users.noreply.github.com>
2024-10-23 11:32:54 +0200

    UPDATE footer

--- footer.php
+++ footer.php
@@ -1,6 +1,8 @@
 <footer>
   <nav>
-    <a class="nav-footer-btn" href="">Mentions légales</a>
-    <a href=""></a>
+    <div>
+      <a class="nav-footer-btn" href="">Mentions légales</a>
+    </div>
+    <p>&#169; 2024 - Florian CHICOT - Tous droits réservés</p>
   </nav>
 </footer>
