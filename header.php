90a00fb57e99904e4cc740c70203b706aff6b321
Florian <105889597+florian-chicot@users.noreply.github.com>
2024-10-23 11:06:33 +0200

    UPDATE header : Refactoring and styling

--- header.php
+++ header.php
@@ -1,12 +1,12 @@
 <header>
   <nav>
     <h1>MES MATCHS</h1>
-    <ul id="nav-menu">
-      <li><a href="index.php">Accueil</a></li>
-      <li><a href="matches-list.php">Description</a></li>
-      <li><a href="contact.php">Contact</a></li>
-    </ul>
-    <span class="checkbtn" onclick="toggleMenu()">&#9776;</span>
+    <div id="nav-menu">
+      <a href="index.php">Accueil</a>
+      <a href="matches-list.php">Description</a>
+      <a href="contact.php">Contact</a>
+    </div>
+    <span class="burger-menu-btn" onclick="toggleMenu()">&#9776;</span>
   </nav>
 </header>
 <script>
