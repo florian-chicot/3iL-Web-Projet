2ec69df6f1ac7d72d06af238446c32aca58a6647
Florian <105889597+florian-chicot@users.noreply.github.com>
2024-10-28 16:17:24 +0100

    UPDATE Header and footer to match with new name and logo

--- view/.include/Footer.php
+++ view/.include/Footer.php
@@ -3,6 +3,6 @@
     <div>
       <a class="nav-footer-btn" href="">Mentions légales</a>
     </div>
-    <p>&#169; 2024 - Florian CHICOT - Tous droits réservés</p>
+    <p>&#169; 2024 - MatchMemories - Tous droits réservés</p>
   </nav>
 </footer>
--- view/.include/Header.php
+++ view/.include/Header.php
@@ -1,17 +1,17 @@
 <header>
   <nav>
-    <h1><a href="index.php">MES MATCHS</a></h1>
+    <h1><a href="index.php"><img class="logo-img" src="logo.svg"></a></h1>
     <div id="nav-menu">
       <a href="index.php">Accueil</a>
       <a href="?controller=ListOfMatches&action=descriptionRoute">Description</a>
-      <a href="contact.php">Contact</a>
+
+      <?php if (isset($_SESSION['user'])): ?>
+      <a href="index.php?controller=Login&action=logout">Se déconnecter</a>
+      <?php else: ?>
+      <a href="index.php?controller=Login&action=loginForm">Se connecter</a>
+      <?php endif; ?>
     </div>
     <span class="burger-menu-btn" onclick="toggleMenu()">&#9776;</span>
   </nav>
 </header>
-<script>
-function toggleMenu() {
-  var menu = document.getElementById('nav-menu');
-  menu.classList.toggle('show');
-}
-</script>
+<script src="assets/scripts/header.js"></script>
