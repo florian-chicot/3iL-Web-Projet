f3026be62161a2c74b3a945891ffe6ea0ffd61b7
Florian <105889597+florian-chicot@users.noreply.github.com>
2024-10-28 16:48:53 +0100

    ADD favicon

--- view/Base.php
+++ view/Base.php
@@ -4,6 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title><?php echo $pagetitle ?></title>
+  <link rel="shortcut icon" href="favicon.ico" type="image/x-icon"/>
   <link rel="stylesheet" href="assets/styles/style.css">
   <link rel="stylesheet" href="assets/styles/media-queries.css">
   <link rel="stylesheet" href="assets/styles/carousel.css">
