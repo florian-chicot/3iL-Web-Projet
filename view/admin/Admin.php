677f9fad359129762dbd61f86508c5df2157a6f8
Florian <105889597+florian-chicot@users.noreply.github.com>
2024-11-01 12:42:19 +0100

    UPDATE style for Admin.php

--- view/admin/Admin.php
+++ view/admin/Admin.php
@@ -1,12 +1,11 @@
-<section>
-  <h2>Administration</h2>
-  <p>Bienvenue dans l'interface d'administration. Choisissez une entité à gérer :</p>
-  <ul>
-    <a href="index.php?controller=AdminCompetition&action=readAll">Gérer les Compétitions</a>
-    <a href="index.php?controller=Match&action=readAll">Gérer les Matchs</a>
-    <a href="index.php?controller=Sport&action=readAll">Gérer les Sports</a>
-    <a href="index.php?controller=Stadium&action=readAll">Gérer les Stades</a>
-    <a href="index.php?controller=Team&action=readAll">Gérer les Équipes</a>
-    <a href="index.php?controller=User&action=readAll">Gérer les Utilisateurs</a>
+<section class="admin-section">
+  <h2 class="admin-title">Administration</h2>
+  <ul class="admin-links">
+    <a href="index.php?controller=AdminCompetition&action=readAll" class="admin-link">Gérer les Compétitions</a>
+    <a href="index.php?controller=Match&action=readAll" class="admin-link">Gérer les Matchs</a>
+    <a href="index.php?controller=Sport&action=readAll" class="admin-link">Gérer les Sports</a>
+    <a href="index.php?controller=Stadium&action=readAll" class="admin-link">Gérer les Stades</a>
+    <a href="index.php?controller=Team&action=readAll" class="admin-link">Gérer les Équipes</a>
+    <a href="index.php?controller=User&action=readAll" class="admin-link">Gérer les Utilisateurs</a>
   </ul>
 </section>
