fd287417957be19bace01c99ed1c82d269bbd79c
Florian <105889597+florian-chicot@users.noreply.github.com>
2024-11-02 22:27:22 +0100

    UPDATE Footer

--- view/.include/Footer.php
+++ view/.include/Footer.php
@@ -2,7 +2,8 @@
   <nav>
     <div>
       <a class="nav-footer-btn" href="">Mentions légales</a>
+      <a class="nav-footer-btn" href="">Contact</a>
     </div>
-    <p>&#169; 2024 - MatchMemories - Tous droits réservés</p>
+    <p>&#169; <?php echo date("Y")?> - MatchMemories - Tous droits réservés</p>
   </nav>
 </footer>
