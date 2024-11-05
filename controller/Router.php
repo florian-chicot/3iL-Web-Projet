1eb181abc618fe2ba4b1d2ffd759f3c4c2de9bb7
Florian <105889597+florian-chicot@users.noreply.github.com>
2024-11-05 14:28:39 +0100

    UPDATE Router

--- controller/Router.php
+++ controller/Router.php
@@ -9,11 +9,7 @@ require_once File::build_path(array("controller", "ControllerMatch.php"));
 require_once File::build_path(array("controller", "ControllerSignup.php"));
 
 if (!isset($_GET['action'])) {
-  $controller = 'home';
-  $view = 'Home';
-  $pagetitle = 'Accueil';
-  $description ='Accueil du site web';
-  require File::build_path(array("view", "Base.php"));   
+  ControllerHome::homeRoute();
 } else {
     $action = $_GET['action'];
   if (!isset($_GET['controller'])) {
@@ -25,8 +21,7 @@ if (!isset($_GET['action'])) {
   $controller_class = 'Controller' . ucfirst($controller);
 
   if (class_exists($controller_class)) {
-    $allMethodsController = get_class_methods($controller_class);
-    if (in_array($action, $allMethodsController)) {
+    if (in_array($action,  get_class_methods($controller_class))) {
       $controller_class::$action();
     } else {
         ControllerError::errorRoute();
