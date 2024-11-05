d6feb4ffdc0a14ddd38ff50be302ed3ed7cabfac
Florian <105889597+florian-chicot@users.noreply.github.com>
2024-11-05 23:51:15 +0100

    UPDATE minified

--- view/Base.php
+++ view/Base.php
@@ -5,8 +5,8 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title><?php echo $pagetitle ?></title>
   <link rel="shortcut icon" href="favicon.ico" type="image/x-icon"/>
-  <link rel="stylesheet" href="assets/styles/style.css">
-  <link rel="stylesheet" href="assets/styles/media-queries.css">
+  <link rel="stylesheet" href="assets/styles/style.min.css">
+  <link rel="stylesheet" href="assets/styles/media-queries.min.css">
   <?php
     if(isset($stylesheets)){
       foreach ($stylesheets as $stylesheet){
