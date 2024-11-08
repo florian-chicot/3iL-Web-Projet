d35a303d0ee4326540adbbf85c9aaf62f1948715
Florian <105889597+florian-chicot@users.noreply.github.com>
2024-11-08 23:38:54 +0100

    UPDATE admin.php

--- view/admin/Admin.php
+++ view/admin/Admin.php
@@ -2,10 +2,10 @@
   <h2 class="admin-title">Administration</h2>
   <ul class="admin-links">
     <a href="index.php?controller=AdminCompetition&action=readAll" class="admin-link">Gérer les Compétitions</a>
-    <a href="index.php?controller=Match&action=readAll" class="admin-link">Gérer les Matchs</a>
-    <a href="index.php?controller=Sport&action=readAll" class="admin-link">Gérer les Sports</a>
-    <a href="index.php?controller=Stadium&action=readAll" class="admin-link">Gérer les Stades</a>
-    <a href="index.php?controller=Team&action=readAll" class="admin-link">Gérer les Équipes</a>
-    <a href="index.php?controller=User&action=readAll" class="admin-link">Gérer les Utilisateurs</a>
+    <a href="#" class="admin-link">Gérer les Matchs</a>
+    <a href="#" class="admin-link">Gérer les Sports</a>
+    <a href="#" class="admin-link">Gérer les Stades</a>
+    <a href="#" class="admin-link">Gérer les Équipes</a>
+    <a href="#" class="admin-link">Gérer les Utilisateurs</a>
   </ul>
 </section>
